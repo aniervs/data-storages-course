898defbee31c406e69e6d2aaf3184ec5724c84c6
Anier Velasco Sotomayor <anier.velasco@gmail.com>
2023-05-12 18:09:22 +0200

    Call drop_tables in main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from src.utils import create_tables, seed_tables, ensure_schema
+from src.utils import create_tables, seed_tables, ensure_schema, drop_tables
 
 
 if __name__ == '__main__':
     schema = ensure_schema(drop_if_exists=False)
+    drop_tables(schema)
     create_tables(schema)
     seed_tables(schema)
+
+
+
+
